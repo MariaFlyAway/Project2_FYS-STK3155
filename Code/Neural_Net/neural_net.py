fe3979a65fa2b746ad4c354245dd9a5c4af4ff90
MariaFlyAway <mariakov@uio.no>
2024-10-13 12:44:18 +0200

    Added some additional activation functions

--- Code/Neural_Net/neural_net.py
+++ Code/Neural_Net/neural_net.py
@@ -1,7 +1,27 @@
-import numpy as np
+import autograd.numpy as np
+
+# Defining some activation functions
+def ReLU(z):
+    return np.where(z > 0, z, 0)
+
 
 def sigmoid(z):
-    return 1.0/(1.0+np.exp(-z))
+    return 1 / (1 + np.exp(-z))
+
+
+def softmax(z):
+    """Compute softmax values for each set of scores in the rows of the matrix z.
+    Used with batched input data."""
+    e_z = np.exp(z - np.max(z, axis=0))
+    return e_z / np.sum(e_z, axis=1)[:, np.newaxis]
+
+
+def softmax_vec(z):
+    """Compute softmax values for each set of scores in the vector z.
+    Use this function when you use the activation function on one vector at a time"""
+    e_z = np.exp(z - np.max(z))
+    return e_z / np.sum(e_z)
+
 
 class NeuralNet:
     def __init__(self, n_features, n_hidden, n_classes, random_state):
